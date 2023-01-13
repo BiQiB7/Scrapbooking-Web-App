31f274a16cf5c15ba4845ead4b6e3fb90971f4da
BiQiB7 <fongbiqi@gmail.com>
2023-01-13 23:54:23 +0800

    added login and registration

--- scrappAR/main/urls.py
+++ scrappAR/main/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, re_path
-from . import views
+from django.urls import path, include
+from users import views as usersview
+from main import views as mainview
 from django.urls import re_path as url
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', mainview.home, name='home'),
+    path('login/', usersview.user_login, name='login'),
+    path('register/', usersview.user_registration, name='register')
 ]
--- scrappAR/users/views.py
+++ scrappAR/users/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, UserRegistrationFrom
+from django.http import HttpResponse
 # Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request,username=data['username'],password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("user authenticaed and logged in")
+            else:
+                return HttpResponse('Invalid credentials')
+    else: 
+        form = LoginForm()
+    return render(request, 'users/login.html',{'form':form})
+
+def user_registration(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationFrom(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit = False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,'users/register_done.html')
+    else:
+        user_form = UserRegistrationFrom()
+    return render(request, 'users/register.html',{'user_form':user_form})
