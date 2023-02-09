4f52aba3eba81411cb7fd24db08a7c0af82c98b0
Bi Qi <biqifong@gmail.com>
2023-02-09 15:24:58 +0800

    demo ready

--- scrappAR/profiles/models.py
+++ scrappAR/profiles/models.py
@@ -4,10 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
     
 class Image(models.Model):
-   caption=models.CharField(max_length=100)
-   image=models.ImageField(upload_to="img/%y")
-   
+   followers = models.IntegerField(default = 0)
+   desc = models.CharField(max_length=200, null = True)
+   name = models.CharField(max_length=200, null = True)
+   caption=models.CharField(max_length=100, null = True)
+   image=models.ImageField(upload_to="img/%y", null = True)
+   pic=models.ImageField(upload_to="img/%y", default = "blank.png")
+   following = models.IntegerField(default = 0)
    
    
    def __str__(self):
-    return self.caption 
+    return self.caption
